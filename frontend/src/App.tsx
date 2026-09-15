e30edc0a9dcd143368006db322e2742f0bc4626b
Mirshod Allaberganov <mir.allaberganov@gmail.com>
2026-09-15 06:17:37 +0500

    M05 - wire billing route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,7 @@ import { useAuth } from './auth/useAuth'
 import { AppShell } from './components/AppShell'
 import { LoginPage, RegisterPage } from './pages/AuthPages'
 import { AppointmentsPage } from './pages/AppointmentsPage'
+import { BillingPage } from './pages/BillingPage'
 import { ClientsPage, EmployeesPage, ServicesPage } from './pages/DirectoryPages'
 import { NotificationsPage } from './pages/NotificationsPage'
 import { OverviewPage } from './pages/OverviewPage'
@@ -14,5 +15,5 @@ function ProtectedShell() {
 }
 
 export default function App() {
-  return <AuthProvider><Routes><Route path="/login" element={<LoginPage />} /><Route path="/register" element={<RegisterPage />} /><Route element={<ProtectedShell />}><Route index element={<OverviewPage />} /><Route path="clients" element={<ClientsPage />} /><Route path="employees" element={<EmployeesPage />} /><Route path="services" element={<ServicesPage />} /><Route path="appointments" element={<AppointmentsPage />} /><Route path="notifications" element={<NotificationsPage />} /></Route><Route path="*" element={<Navigate to="/" replace />} /></Routes></AuthProvider>
+  return <AuthProvider><Routes><Route path="/login" element={<LoginPage />} /><Route path="/register" element={<RegisterPage />} /><Route element={<ProtectedShell />}><Route index element={<OverviewPage />} /><Route path="clients" element={<ClientsPage />} /><Route path="employees" element={<EmployeesPage />} /><Route path="services" element={<ServicesPage />} /><Route path="appointments" element={<AppointmentsPage />} /><Route path="notifications" element={<NotificationsPage />} /><Route path="billing" element={<BillingPage />} /></Route><Route path="*" element={<Navigate to="/" replace />} /></Routes></AuthProvider>
 }
